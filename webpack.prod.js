50c60bbb457f4ac767423bacd86c7d58945ebde5
Kevinabcabc <1356075038@qq.com>
2020-02-25 00:09:28 +0800

    1

---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+let {smart} = require('webpack-merge')
+let base = require ('./webpack.base.js')
+
+module.exports = smart(base,{
+    mode:'production',//development  
+    optimization: {//开发环境压缩
+        minimizer: {
+
+        }
+    },
+    plugins:[
+        
+    ]
+})
